8982824a95c4c9c8956973eb0825ad94d52e62a7
josephsmendoza <josephsmendoza@gmail.com>
2021-02-04 10:56:10 +0000

    add verbosity for troubleshooting

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,4 +1,5 @@
 
+    $ErrorActionPreference = 'Continue'
     if($IsWindows){
         $env:PATH += ";/tools/ldc/bin"
         $cliext=".exe"
@@ -9,12 +10,16 @@
     }
     "flink", "rflink", "uflink", "smerge" | ForEach-Object {
         dub build :$_ -b release -c lib --parallel
+        echo "uploading util/$_/$libpre$_$libext"
         Push-AppveyorArtifact util/$_/$libpre$_$libext
         dub build :$_ -b release -c cli --parallel
+        echo uploading "util/$_/$_$cliext"
         Push-AppveyorArtifact util/$_/$_$cliext
     }
     $_="fpm"
     dub build -b release -c lib --parallel
+    echo "uploading $libpre$_$cliext"
     Push-AppveyorArtifact $libpre$_$cliext
     dub build -b release -c cli --parallel
+    echo "uploading $_$cliext"
     Push-AppveyorArtifact $_$cliext
